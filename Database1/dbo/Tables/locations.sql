427ca3bb95be16404e120fee488839c476a32f98
DharmbirSinghGit <68269947+DharmbirSinghGit@users.noreply.github.com>
2024-02-01 21:02:02 +0530

    Db

---
+++ Database1/dbo/Tables/locations.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[locations] (
+    [Id]        NVARCHAR (450) NOT NULL,
+    [Name]      NVARCHAR (MAX) NOT NULL,
+    [StartTime] DATETIME2 (7)  NOT NULL,
+    [EndTime]   DATETIME2 (7)  NOT NULL,
+    CONSTRAINT [PK_locations] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
